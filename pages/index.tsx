cfee03c370235b3ab082a8124fd6fd59e0feff6e
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-16 00:27:36 +0800

    decent stuff...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex-auto text-center container mx-auto max-w-fit bg-gradient-to-r from-black to-slate-950 min-w-full min-h-screen p-4 px-4 py-4">
+    <div className="flex-auto text-center container mx-auto max-w-fit bg-gradient-to-r from-black to-slate-900 min-w-full min-h-screen p-4 px-4 py-4">
       <div className="mx-auto border-[2.5px] border-l-teal-400 border-t-emerald-500 border-r-cyan-400 border-b-sky-400 rounded-xl min-h-screen p-4 py-2">
         <h1><strong className="text-6xl text-purple-500 font-light">Hey <span className="text-pink-500">there!</span></strong></h1>
-        <hr className="mt-2 mb-3.5 bg-gradient-to-r from-indigo-500 via-teal-500 to-pink-500 border-0 h-[4.5px] rounded-xl" />
+        <hr className="mt-3.5 mb-3.5 bg-gradient-to-r from-indigo-500 via-teal-500 to-pink-500 border-0 h-[4.5px] rounded-xl" />
         <p className={"text-3xl bg-gradient-to-r from-violet-500 via-pink-500 to-yellow-500 text-transparent text-clip bg-clip-text"}><strong>This is a test text for using new things that I've learnt.</strong></p>
+        <p className={"text-3xl bg-gradient-to-r from-purple-500 via-pink-500 to-slate-500 text-transparent text-clip bg-clip-text"}><strong>This is built purely using NextJS and Tailwind CSS.</strong></p>
       </div>
     </div>
   )
