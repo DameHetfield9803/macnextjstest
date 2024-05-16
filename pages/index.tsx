95179848c99ecfb42fa76ff08356d39227b94c9d
DameHetfield9803 <damiendkfoo@yahoo.com.sg>
2024-05-16 17:04:04 +0800

    padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,9 @@ import { TypeAnimation } from "react-type-animation";
 export default function Home() {
   return (
     <div className="flex-auto text-center container mx-auto max-w-fit bg-black min-w-full min-h-screen p-4 px-6 py-4">
-      <div className="mx-auto border-[2.5px] border-l-teal-400 border-t-emerald-500 border-r-cyan-400 border-b-sky-400 rounded-xl text-center min-h-screen p-4 py-3">
+      <div className="mx-auto border-[2.5px] border-l-teal-400 border-t-emerald-400 border-r-cyan-400 border-b-sky-400 rounded-xl text-center min-h-screen p-4 py-2">
         <h1><strong className="text-7xl text-purple-500 font-light">Hey <span className="text-pink-500">there!</span></strong></h1>
-        <hr className="mt-3.5 mb-3.5 bg-gradient-to-r from-indigo-500 via-teal-500 to-pink-500 border-0 h-[4.5px] rounded-xl" />
+        <hr className="mt-3.5 mb-3.5 bg-gradient-to-r from-violet-500 via-pink-500 to-yellow-500 border-0 h-[4.5px] rounded-xl" />
         <p className={"text-3xl bg-gradient-to-r from-violet-500 via-pink-500 to-yellow-500 text-transparent text-clip bg-clip-text"}><strong>This is a test text for using new things that I've learnt.</strong></p>
         <p className={"text-3xl bg-gradient-to-r from-purple-500 via-pink-500 to-slate-500 text-transparent text-clip bg-clip-text"}><strong>This is built using NextJS and Tailwind CSS.</strong></p>
         <p><strong className="text-xl bg-gradient-to-r from-pink-500 via-cyan-500 to-emerald-500 text-transparent bg-clip-text">
